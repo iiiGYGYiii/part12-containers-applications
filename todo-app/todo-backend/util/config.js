8b579dce18fddec50b46bcdb4b2e09b4f8e53449
iiiGYGYiii <ococoletzic@gmail.com>
2021-11-05 18:33:39 -0600

    Exercise 12.15

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,7 +1,10 @@
-const MONGO_URL = process.env.MONGO_URL || undefined
-const REDIS_URL = process.env.REDIS_URL || undefined
-
+const MONGO_URL = process.env.MONGO_URL || undefined;
+const REDIS_URL = process.env.REDIS_URL || undefined;
+/*
+  MONGO_URL=mongodb://the_username:complex-example@localhost:3456/the_database
+  REDIS_URL=//localhost:5001
+*/
 module.exports = {
-  MONGO_URL,//: 'mongodb://the_username:the_password@localhost:3456/the_database',
-  REDIS_URL//: '//localhost:6378'
-}
+  MONGO_URL,
+  REDIS_URL,
+};
--- todo-app/todo-frontend/src/App.js
+++ todo-app/todo-frontend/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-import TodoView from './Todos/TodoView'
+import "./App.css";
+import TodoView from "./Todos/TodoView";
 
 function App() {
   return (
     <div className="App">
+      <h1>DESMADRE</h1>
       <TodoView />
     </div>
   );
